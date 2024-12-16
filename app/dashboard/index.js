8190557332e7714ff56f545b43339c0fbb08cdb2
unknown <erdematik8@gmail.com>
2024-12-16 18:47:33 +0300

    added cookies feature

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -1,61 +1,61 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Platform, View, Text, StyleSheet } from "react-native";
 import { AppContext } from "../../context/AppContext";
 import * as SecureStore from "expo-secure-store";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Cookies from "js-cookie";
 
-const storeData = async (value) => {
-  try {
-    await AsyncStorage.setItem("my-key", value);
-  } catch (e) {
-    // saving error
-  }
+const GetCookie = () => {
+  alert(Cookies.get("my-key"));
 };
 
 export default function Dashboard() {
   const { user } = useContext(AppContext);
+  // const [token, setToken] = useState("");
+  // const [error, setError] = useState(null);
+
   console.log(user?.token || "No Token Available");
 
-  if (Platform.OS === "web") {
-    console.log("Web Platform");
-    async function save(key, value) {
-      console.log(key, value);
-      localStorage.setItem(key, value);
-    }
-    save("token", user?.token || "No Token Available");
-    async function getValueFor(key) {
-      let result = localStorage.getItem(key);
-      if (result) {
-        console.log(key);
-        alert("🔐 Here's your value 🔐 \n" + result);
-      } else {
-        alert("No values stored under that key.");
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      console.log("Web Platform");
+      const storeData = async (value) => {
+        try {
+          const jsonValue = JSON.stringify(value);
+          await AsyncStorage.setItem("my-key", jsonValue);
+          Cookies.set("my-key", value);
+        } catch (e) {
+          // saving error
+        }
+      };
+      if (user?.token) storeData(user.token); // save tg
+      else return;
+    } else {
+      async function save(key, value) {
+        await SecureStore.setItemAsync(key, value);
       }
-    }
-  } else {
-    async function save(key, value) {
-      await SecureStore.setItemAsync(key, value);
-    }
-    save("token", user?.token || "No Token Available");
+      save("token", user?.token || "No Token Available");
 
-    async function getValueFor(key) {
-      let result = await SecureStore.getItemAsync(key);
-      if (result) {
-        alert("🔐 Here's your value 🔐 \n" + result);
-      } else {
-        alert("No values stored under that key.");
+      async function getValueFor(key) {
+        let result = await SecureStore.getItemAsync(key);
+        if (result) {
+          alert("🔐 Here's your value 🔐 \n" + result);
+        } else {
+          alert("No values stored under that key.");
+        }
       }
-    }
 
-    console.log("Mobile Platform");
-  }
+      console.log("Mobile Platform");
+    }
+  }, [user?.token]);
 
   // getValueFor("token");
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, {user?.username || "Guest"}!</Text>
-      <Text>Your token: {user?.token || "No Token Available"}</Text>
+      {/* <Text>Your token: {user?.token || "No Token Available"}</Text> */}
+      <button onClick={GetCookie}>Get Cookie</button>
 
       {/* Display other user-related data */}
     </View>
