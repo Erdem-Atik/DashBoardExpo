7ed8c273bb74a0a8e39ed3c7ef54bfdd530ccb32
unknown <erdematik8@gmail.com>
2024-12-18 18:22:27 +0300

    edited

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -1,8 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Platform, View, Text, StyleSheet } from "react-native";
 import { AppContext } from "../../context/AppContext";
-import * as SecureStore from "expo-secure-store";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import Cookies from "js-cookie";
 import { createProject, getProjects } from "../../api/projects";
 
@@ -18,45 +16,21 @@ export default function Dashboard() {
   useEffect(() => {
     if (Platform.OS === "web") {
       console.log("Web Platform");
-      const storeData = async (value) => {
-        try {
-          const jsonValue = JSON.stringify(value);
-          await AsyncStorage.setItem("my-key", jsonValue);
-          Cookies.set("my-key", value);
-        } catch (e) {
-          // saving error
-        }
-      };
+
       if (user?.token) {
-        storeData(user.token); // save tg
         const newProject = {
-          name: "Örnek Proje",
+          name: "instance-1",
           description: "erdem 1.1.1",
           startDate: "2024-01-01",
           endDate: "2024-12-31",
         };
+
         const createResult = createProject(user.token, newProject);
         console.log("Proje oluşturma sonucu:", createResult);
 
         const projects = getProjects(user.token);
         console.log("Proje listesi:", projects);
       } else return;
-    } else {
-      async function save(key, value) {
-        await SecureStore.setItemAsync(key, value);
-      }
-      save("token", user?.token || "No Token Available");
-
-      async function getValueFor(key) {
-        let result = await SecureStore.getItemAsync(key);
-        if (result) {
-          alert("🔐 Here's your value 🔐 \n" + result);
-        } else {
-          alert("No values stored under that key.");
-        }
-      }
-
-      console.log("Mobile Platform");
     }
   }, [user?.token]);
 
